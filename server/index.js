b1a4bb6b21acc1b109f6e27ef4318c33d2e5f16a
Deepak7447 <deepakdwivedi7249@gmail.com>
2026-06-25 01:16:23 +0530

    checked weather endpoint api working

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ console.log("My weather api key is : "+ process.env.WEATHER_API_KEY);
 const express=require("express");
 const app=express();
 
+
+
+app.get('/api/weather',(req,res)=>{
+    // Handle weather API request
+    res.json({
+        message:"weather api endpoint is working properly"
+    })
+});
+
+
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
